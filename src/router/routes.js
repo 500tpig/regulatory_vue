66abfb54e52770f378ce4609482232b1890ceacf
500tpig <995375558@qq.com>
2020-10-15 18:03:21 +0800

    添加login页面。

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,13 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '', component: () => import('pages/Index.vue')
+      }
     ]
+  },
+  {
+    path: '/login', component: () => import('pages/login/index.vue')
   },
 
   // Always leave this as last one,
