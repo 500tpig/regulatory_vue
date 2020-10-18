2074ed649720b8ebca59d7de2f61bafd7a43307c
500tpig <995375558@qq.com>
2020-10-18 14:27:22 +0800

    引入echarts。

--- src/boot/components.js
+++ src/boot/components.js
@@ -1,8 +1,10 @@
 // import something here
 import SlideVerify from "vue-monoplasty-slide-verify";
+import echarts from "echarts";
 // "async" is optional;
 // more info on params: https://quasar.dev/quasar-cli/cli-documentation/boot-files#Anatomy-of-a-boot-file
 export default async ({ Vue }) => {
   // something to do
   Vue.use(SlideVerify);
+  Vue.prototype.$echarts = echarts;
 };
