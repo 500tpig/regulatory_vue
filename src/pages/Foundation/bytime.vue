8d7fd91be1e87860bc4439cd704a726b046e83a4
500tpig <995375558@qq.com>
2020-10-18 14:29:28 +0800

    添加基金使用概况按时间页面。

--- src/pages/Foundation/bytime.vue
+++ src/pages/Foundation/bytime.vue
@@ -1,9 +1,68 @@
 <template>
-  <q-page class="flex flex-center">通过时间 </q-page>
+  <q-page class="flex flex-center">
+    <div id="main"></div>
+  </q-page>
 </template>
 
 <script>
-export default {};
+import { EleResize } from "assets/js/esresize";
+export default {
+  name: "Echarts",
+  methods: {
+    drawLine(dom, option) {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById(dom));
+      let resizeDiv = document.getElementById(dom);
+      // 绘制图表
+      myChart.setOption(option);
+      console.log(EleResize);
+      let listener = function() {
+        myChart.resize();
+      };
+      EleResize.on(resizeDiv, listener);
+    },
+    myEcharts() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("main"));
+      let resizeDiv = document.getElementById("main");
+      // 指定图表的配置项和数据
+      let option = {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+      let linstener = function() {
+        myChart.resize();
+      };
+      EleResize.on(resizeDiv, linstener);
+    }
+  },
+  mounted() {
+    this.myEcharts();
+  }
+};
 </script>
 
-<style></style>
+<style lang="sass" scoped>
+#main
+  width: 50%
+  height: 300px
+</style>
