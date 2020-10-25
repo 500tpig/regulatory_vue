29366418918737c9147c89b36a1ee5138aa5dcf8
500tpig <995375558@qq.com>
2020-10-25 18:41:19 +0800

    基本完成按时间页面。

--- src/assets/js/charts/byTimeOption.js
+++ src/assets/js/charts/byTimeOption.js
@@ -1,187 +1,201 @@
-const byTimeOption = {
-  title: {
-    // text: "ECharts 入门示例"
-  },
-  toolbox: {
-    //可视化的工具箱
-    show: true,
-    right: "15%",
-    top: "2%",
-    feature: {
-      dataView: {
-        //数据视图
-        show: true,
-        readOnly: true,
-        optionToContent: function(opt) {
-          let axisData = opt.xAxis[0].data;
-          let series = opt.series;
-          let tdHeaders = "<td>日期</td>"; //表头
-          series.forEach(function(item) {
-            tdHeaders += "<td>" + item.name + "</td>"; //组装表头
-          });
-          let table =
-            '<div class="table-responsive"><table class="table table-bordered table-striped table-hover" style="text-align:center;color:#222831;"><tbody><tr>' +
-            tdHeaders +
-            "</tr>";
-          let tdBodys = ""; //数据
-          for (let i = 0, l = axisData.length; i < l; i++) {
-            for (let j = 0; j < series.length; j++) {
-              tdBodys += "<td>" + series[j].data[i] + "元</td>"; //组装表数据
-            }
-            table +=
-              '<tr><td style="padding: 0 10px">' +
-              axisData[i] +
-              "</td>" +
-              tdBodys +
+import { accAdd } from "../util/common";
+function setbyTimeChartOption(chartData) {
+  let chargingTime = [];
+  let individualPay = [];
+  let totalCost = [];
+  let medicarePay = [];
+  chartData.map(item => {
+    chargingTime.push(
+      item.chargingTime.slice(0, 4) + "-" + item.chargingTime.slice(4)
+    );
+    individualPay.push(item.individualPay);
+    totalCost.push(item.totalCost);
+    medicarePay.push(item.medicarePay);
+  });
+  let byTimeOption = {
+    title: {
+      // text: "ECharts 入门示例"
+    },
+    toolbox: {
+      //可视化的工具箱
+      show: true,
+      right: "15%",
+      top: "2%",
+      feature: {
+        dataView: {
+          //数据视图
+          show: true,
+          readOnly: true,
+          optionToContent: function(opt) {
+            let axisData = opt.xAxis[0].data;
+            let series = opt.series;
+            let tdHeaders = "<td>日期</td>"; //表头
+            series.forEach(function(item) {
+              tdHeaders += "<td>" + item.name + "</td>"; //组装表头
+            });
+            let table =
+              '<div class="table-responsive"><table class="table table-bordered table-striped table-hover" style="text-align:center;color:#222831;"><tbody><tr>' +
+              tdHeaders +
               "</tr>";
-            tdBodys = "";
-          }
+            let tdBodys = ""; //数据
+            for (let i = 0, l = axisData.length; i < l; i++) {
+              for (let j = 0; j < series.length; j++) {
+                tdBodys += "<td>" + series[j].data[i] + "元</td>"; //组装表数据
+              }
+              table +=
+                '<tr><td style="padding: 0 10px">' +
+                axisData[i] +
+                "</td>" +
+                tdBodys +
+                "</tr>";
+              tdBodys = "";
+            }
 
-          table += "</tbody></table></div>";
-          return table;
-        }
-      },
-      restore: {
-        //重置
-        show: true
-      },
-      saveAsImage: {
-        //保存图片
-        show: true
-      },
-      magicType: {
-        //动态类型切换
-        type: ["bar", "line"]
-      }
-    }
-  },
-  tooltip: {
-    trigger: "axis",
-    formatter: {
-      _custom: {
-        type: "function",
-        display: "<span>ƒ</span> (params)"
-      }
-    },
-    axisPointer: {
-      label: {
-        type: "cross",
-        show: true,
-        backgroundColor: "#fff",
-        color: "#556677",
-        borderColor: "rgba(0,0,0,0)",
-        shadowColor: "rgba(0,0,0,0)",
-        shadowOffsetY: 0
-      }
-    },
-    backgroundColor: "#fff",
-    textStyle: {
-      color: "#5c6c7c"
-    },
-    padding: [20, 20],
-    extraCssText: "box-shadow: 1px 0 2px 0 rgba(163,163,163,0.5)"
-  },
-  grid: {
-    top: "10%",
-    bottom: "18%"
-    // x: 40,
-    // y: 40,
-    // x2: 40,
-    // y2: 20
-  },
-  dataZoom: [
-    {
-      show: true,
-      start: 0,
-      end: 100,
-      handleSize: "100%",
-      bottom: "5%",
-      // left: "14%",
-      // right: "30%",
-      handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
-      handleStyle: {
-        /*手柄的样式*/
-        color: "#40bcf9",
-        borderColor: "#1fb2fb"
-      },
-      backgroundColor: "#e2f3ff" /*背景 */,
-      dataBackground: {
-        /*数据背景*/
-        lineStyle: {
-          color: "#ff2e63",
-          width: 2
+            table += "</tbody></table></div>";
+            return table;
+          }
         },
-        areaStyle: {
-          color: "#d4d9dd"
-        }
-      },
-      fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/
-    },
-    {
-      show: true,
-      type: "inside",
-      start: 0,
-      end: 100
-    },
-    {
-      handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
-      handleStyle: {
-        /*手柄的样式*/
-        color: "#40bcf9",
-        borderColor: "#1fb2fb"
-      },
-      backgroundColor: "#e2f3ff" /*背景 */,
-      dataBackground: {
-        /*数据背景*/
-        lineStyle: {
-          color: "#000000"
+        restore: {
+          //重置
+          show: true
         },
-        areaStyle: {
-          color: "#d4d9dd"
+        saveAsImage: {
+          //保存图片
+          show: true
+        },
+        magicType: {
+          //动态类型切换
+          type: ["bar", "line"]
         }
-      },
-      fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/,
-      show: true,
-      yAxisIndex: 0,
-      filterMode: "empty",
-      width: 20,
-      height: "70%",
-      showDataShadow: false,
-      right: "4%",
-      top: "13%"
-    }
-  ],
-  legend: {
-    data: ["个人支付", "医保支付", "医疗总费用"],
-    x: "center",
-    top: 10
-  },
-  xAxis: {
-    type: "category",
-    data: ["2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06"],
-    axisLine: {
-      lineStyle: {
-        color: "#DCE2E8"
       }
     },
-    axisTick: {
-      show: false
-    },
-    axisLabel: {
-      interval: 0,
+    tooltip: {
+      trigger: "axis",
+      formatter: {
+        _custom: {
+          type: "function",
+          display: "<span>ƒ</span> (params)"
+        }
+      },
+      axisPointer: {
+        label: {
+          type: "cross",
+          show: true,
+          backgroundColor: "#fff",
+          color: "#556677",
+          borderColor: "rgba(0,0,0,0)",
+          shadowColor: "rgba(0,0,0,0)",
+          shadowOffsetY: 0
+        }
+      },
+      backgroundColor: "#fff",
       textStyle: {
-        color: "#556677"
+        color: "#5c6c7c"
       },
-      // 默认x轴字体大小
-      fontSize: 12,
-      // margin:文字到x轴的距离
-      margin: 15
+      padding: [20, 20],
+      extraCssText: "box-shadow: 1px 0 2px 0 rgba(163,163,163,0.5)"
     },
-    axisPointer: {
-      label: {
-        // padding: [11, 5, 7],
-        padding: [0, 0, 10, 0],
-        /*
+    grid: {
+      top: "10%",
+      bottom: "18%"
+      // x: 40,
+      // y: 40,
+      // x2: 40,
+      // y2: 20
+    },
+    dataZoom: [
+      {
+        show: true,
+        start: 0,
+        end: 100,
+        handleSize: "100%",
+        bottom: "5%",
+        // left: "14%",
+        // right: "30%",
+        handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
+        handleStyle: {
+          /*手柄的样式*/
+          color: "#40bcf9",
+          borderColor: "#1fb2fb"
+        },
+        backgroundColor: "#e2f3ff" /*背景 */,
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: "#ff2e63",
+            width: 2
+          },
+          areaStyle: {
+            color: "#d4d9dd"
+          }
+        },
+        fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/
+      },
+      {
+        show: true,
+        type: "inside",
+        start: 0,
+        end: 100
+      },
+      {
+        handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
+        handleStyle: {
+          /*手柄的样式*/
+          color: "#40bcf9",
+          borderColor: "#1fb2fb"
+        },
+        backgroundColor: "#e2f3ff" /*背景 */,
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: "#000000"
+          },
+          areaStyle: {
+            color: "#d4d9dd"
+          }
+        },
+        fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/,
+        show: true,
+        yAxisIndex: 0,
+        filterMode: "empty",
+        width: 20,
+        height: "70%",
+        showDataShadow: false,
+        right: "4%",
+        top: "13%"
+      }
+    ],
+    legend: {
+      data: ["个人支付", "医保支付", "医疗总费用"],
+      x: "center",
+      top: 10
+    },
+    xAxis: {
+      type: "category",
+      data: chargingTime,
+      axisLine: {
+        lineStyle: {
+          color: "#DCE2E8"
+        }
+      },
+      axisTick: {
+        show: false
+      },
+      axisLabel: {
+        interval: 0,
+        textStyle: {
+          color: "#556677"
+        },
+        // 默认x轴字体大小
+        fontSize: 12,
+        // margin:文字到x轴的距离
+        margin: 15
+      },
+      axisPointer: {
+        label: {
+          // padding: [11, 5, 7],
+          padding: [0, 0, 10, 0],
+          /*
     除了padding[0]建议必须是0之外，其他三项可随意设置
     和CSSpadding相同，[上，右，下，左]
     如果需要下边线超出文字，设左右padding即可，注：左右padding最好相同
@@ -190,97 +204,253 @@ const byTimeOption = {
     如：UI图中文字距下边线距离为7 下边线宽度为2
     则padding: [0, 0, 9, 0]
                 */
-        // 这里的margin和axisLabel的margin要一致!
-        margin: 15,
-        // 移入时的字体大小
-        fontSize: 12,
-        backgroundColor: {
-          type: "linear",
-          x: 0,
-          y: 0,
-          x2: 0,
-          y2: 1,
-          colorStops: [
-            {
-              offset: 0,
-              color: "#fff" // 0% 处的颜色
-            },
-            {
-              // offset: 0.9,
-              offset: 0.86,
-              /*
+          // 这里的margin和axisLabel的margin要一致!
+          margin: 15,
+          // 移入时的字体大小
+          fontSize: 12,
+          backgroundColor: {
+            type: "linear",
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: "#fff" // 0% 处的颜色
+              },
+              {
+                // offset: 0.9,
+                offset: 0.86,
+                /*
                     0.86 = （文字 + 文字距下边线的距离）/（文字 + 文字距下边线的距离 + 下边线的宽度）   
                     */
-              color: "#fff" // 0% 处的颜色
-            },
-            {
-              offset: 0.86,
-              color: "#33c0cd" // 0% 处的颜色
-            },
-            {
-              offset: 1,
-              color: "#33c0cd" // 100% 处的颜色
-            }
-          ],
-          global: false // 缺省为 false
-        }
-      }
-    }
-  },
-  yAxis: [
-    {
-      name: "金额",
-      type: "value"
-    }
-  ],
-  series: [
-    {
-      name: "个人支付",
-      type: "bar",
-      data: [5, 20, 36, 10, 10, 20],
-      barMaxWidth: 40,
-      stack: "one",
-      itemStyle: {
-        normal: {
-          color: "#706AE8"
+                color: "#fff" // 0% 处的颜色
+              },
+              {
+                offset: 0.86,
+                color: "#33c0cd" // 0% 处的颜色
+              },
+              {
+                offset: 1,
+                color: "#33c0cd" // 100% 处的颜色
+              }
+            ],
+            global: false // 缺省为 false
+          }
         }
       }
     },
-    {
-      name: "医保支付",
-      type: "bar",
-      stack: "one",
-      data: [5, 20, 36, 10, 10, 20],
-      itemStyle: {
-        normal: {
-          color: "#61C378"
+    yAxis: [
+      {
+        name: "金额",
+        type: "value",
+        axisLabel: {
+          formatter: function(value, index) {
+            if (value > 100000000)
+              return (value / 100000000).toFixed(2) + "亿元";
+            else if (value > 10000) return value / 10000 + "万元";
+            return value.toFixed(2) + "元";
+          }
         }
       }
-    },
-    {
-      name: "医疗总费用",
-      type: "line",
-      symbolSize: 1,
-      symbol: "circle",
-      itemStyle: {
-        normal: {
-          color: "#D95E5A",
-          barBorderRadius: 0
-        }
-      },
-      lineStyle: {
-        normal: {
-          width: 4,
-          itemStyle: {
-            normal: {
-              color: "#D95E5A"
-            }
+    ],
+    series: [
+      {
+        name: "医保支付",
+        type: "bar",
+        stack: "one",
+        barMaxWidth: 40,
+        data: medicarePay,
+        itemStyle: {
+          normal: {
+            color: "#61C378"
           }
         }
       },
-      data: [10, 40, 72, 20, 20, 40]
-    }
-  ]
-};
+      {
+        name: "个人支付",
+        type: "bar",
+        data: individualPay,
+        barMaxWidth: 40,
+        stack: "one",
+        itemStyle: {
+          normal: {
+            color: "#706AE8",
+            barBorderRadius: [30, 30, 0, 0]
+          }
+        }
+      },
+      {
+        name: "医疗总费用",
+        type: "line",
+        symbolSize: 1,
+        symbol: "circle",
+        barMaxWidth: 40,
+        itemStyle: {
+          normal: {
+            color: "#D95E5A",
+            barBorderRadius: 0
+          }
+        },
+        lineStyle: {
+          normal: {
+            width: 4,
+            itemStyle: {
+              normal: {
+                color: "#D95E5A"
+              }
+            }
+          }
+        },
+        data: totalCost
+      }
+    ]
+  };
+  return byTimeOption;
+}
+function setbyTimePieChartOption(chartData) {
+  console.log(chartData);
+  let totalCost = 0;
+  let medicarePay = 0;
+  let individualPay = 0;
+  chartData.map(item => {
+    totalCost = accAdd(totalCost, item.totalCost);
+    medicarePay = accAdd(medicarePay, item.medicarePay);
+    individualPay = accAdd(individualPay, item.individualPay);
+  });
+  let color = ["#61C378", "#706AE8"];
 
-export { byTimeOption };
+  let option = {
+    color: color,
+    title: {
+      text: "{name|医疗总费用}\n{val|" + totalCost + "}",
+      top: "center",
+      left: "center",
+      textStyle: {
+        rich: {
+          name: {
+            fontSize: 16,
+            fontWeight: "normal",
+            color: "#666666",
+            padding: [10, 0]
+          },
+          val: {
+            fontSize: 24,
+            fontWeight: "bold",
+            color: "#D95E5A"
+          }
+        }
+      }
+    },
+    toolbox: {
+      //可视化的工具箱
+      show: true,
+      right: "2%",
+      top: "2%",
+      feature: {
+        restore: {
+          //重置
+          show: true
+        },
+        saveAsImage: {
+          //保存图片
+          show: true
+        }
+      }
+    },
+    tooltip: {
+      trigger: "item",
+      borderColor: "rgba(255,255,255,.3)",
+      backgroundColor: "rgba(13,5,30,.6)",
+      borderWidth: 1,
+      padding: 5,
+      formatter: function(parms) {
+        var str =
+          parms.marker +
+          "" +
+          parms.data.name +
+          "</br>" +
+          "数量：" +
+          parms.data.value +
+          "元</br>" +
+          "占比：" +
+          parms.percent +
+          "%";
+        return str;
+      }
+    },
+    legend: {
+      type: "scroll",
+      orient: "vertical",
+      left: "2%",
+      align: "auto",
+      top: "middle",
+      data: ["医保支付", "个人支付"]
+    },
+    series: [
+      {
+        type: "pie",
+        radius: ["50%", "75%"],
+        center: ["50%", "50%"],
+        data: [
+          {
+            name: "医保支付",
+            value: medicarePay
+          },
+          {
+            name: "个人支付",
+            value: individualPay
+          }
+        ],
+        // hoverAnimation: false,
+        itemStyle: {
+          normal: {
+            borderColor: "#fff",
+            borderWidth: 2
+          }
+        },
+        labelLine: {
+          normal: {
+            length: 10,
+            length2: 160,
+            lineStyle: {
+              color: "#e6e6e6"
+            }
+          }
+        },
+        label: {
+          normal: {
+            formatter: params => {
+              return (
+                "{icon|●}{name|" +
+                params.name +
+                "}{value|" +
+                (params.value / 100000).toFixed(2) +
+                "万元}"
+              );
+            },
+            padding: [0, -170, 25, -180],
+            rich: {
+              icon: {
+                fontSize: 16
+              },
+              name: {
+                fontSize: 14,
+                padding: [0, 10, 0, 4],
+                color: "#666666"
+              },
+              value: {
+                fontSize: 18,
+                fontWeight: "bold",
+                color: "#333333"
+              }
+            }
+          }
+        }
+      }
+    ]
+  };
+  return option;
+}
+export { setbyTimeChartOption, setbyTimePieChartOption };
--- src/pages/Foundation/bytime.vue
+++ src/pages/Foundation/bytime.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="byTIme q-pt-lg">
+  <q-page class="byTIme q-py-lg">
     <div class="row byTime-searchBar-row">
       <!-- 筛选栏 -->
       <div class="col-8 offset-2 byTime-searchBar-row-col">
@@ -7,56 +7,40 @@
           class="byTime-searchBar-row-col-card bg-white text-primary q-pt-md"
         >
           <!-- <q-separator dark /> -->
-          <q-card-section horizontal class="row q-px-lg items-center">
-            <div class=" byTime-searchBar-row-col-card-select">
-              <q-select
-                v-model="searchParam.type"
-                :options="options"
-                multiple
-                rounded
-                outlined
-                prefix="类型"
-                color="primary"
-                bg-color="white"
+          <q-card-section horizontal class="row items-center justify-center">
+            <span class="q-mr-sm">类型选择 :</span>
+            <el-select
+              v-model="searchParam.type"
+              multiple
+              placeholder="请选择"
+              class="col-4"
+            >
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
               >
-                <template v-slot:selected-item="scope">
-                  <q-chip
-                    removable
-                    dense
-                    @remove="scope.removeAtIndex(scope.index)"
-                    :tabindex="scope.tabindex"
-                    color="white"
-                    text-color="primary"
-                    class="q-mx-xs"
-                  >
-                    {{ scope.opt }}
-                  </q-chip>
-                </template>
-              </q-select>
-            </div>
-            <div class=" q-ml-lg">
-              <q-input
-                v-model="searchParam.startDate"
-                prefix="开始日期 :"
-                type="date"
-                rounded
-                outlined
-                :input-style="{ color: '#018DA7' }"
-              />
-            </div>
-            <div class=" q-ml-lg">
-              <q-input
-                v-model="searchParam.endDate"
-                prefix="结束日期 :"
-                type="date"
-                rounded
-                outlined
-                :input-style="{ color: '#018DA7' }"
-              />
+              </el-option>
+            </el-select>
+            <div>
+              <span class="q-ml-lg q-mr-sm">日期选择 :</span>
+              <el-date-picker
+                align="center"
+                value-format="yyyyMMdd"
+                v-model="searchParam.chargingTime"
+                type="monthrange"
+                unlink-panels
+                clearable
+                start-placeholder="开始月份"
+                end-placeholder="结束月份"
+                :picker-options="pickerOptions"
+              >
+              </el-date-picker>
             </div>
           </q-card-section>
           <q-card-actions align="right">
-            <q-btn class="q-mr-lg" flat>提交</q-btn>
+            <q-btn class="q-mr-lg" flat @click="submit">提交</q-btn>
           </q-card-actions>
         </q-card>
       </div>
@@ -64,60 +48,224 @@
     <div class="row">
       <div class="col-8 offset-2 ">
         <q-card class="q-mt-lg q-pt-md">
-          <div id="main"></div>
+          <div ref="histogram" id="byTimeHistogram"></div>
         </q-card>
+      </div>
+    </div>
+    <div class="row q-mt-lg">
+      <q-card class="col-5 offset-1">
+        <div id="byTimePieChart"></div>
+      </q-card>
+      <div>
+        <q-table
+          class="byTimeTable col-5 q-ml-lg shadow-2"
+          title="医保支付详情表"
+          :data="byTimeTable.data"
+          :columns="byTimeTable.columns"
+          row-key="index"
+          flat
+          bordered
+        />
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { EleResize } from "assets/js/esresize";
-import { byTimeOption } from "assets/js/charts/byTimeOption";
+import { EleResize } from "assets/js/util/esresize";
+import { getMonthLastDay } from "assets/js/util/common";
+import {
+  setbyTimeChartOption,
+  setbyTimePieChartOption
+} from "assets/js/charts/byTimeOption";
 export default {
-  name: "Echarts",
+  name: "byTime",
   data() {
     return {
       searchParam: {
-        startDate: "",
-        endDate: "",
-        type: ["个人支付", "医保支付", "医疗总费用"]
+        chargingTime: ["20160701", "20161231"],
+        type: ["individualPay", "medicarePay", "totalCost"]
       },
-      options: ["个人支付", "医保支付", "医疗总费用"],
-      model: {
-        label: "Google",
-        value: "goog",
-        icon: "mail"
+      byTimeTable: {
+        columns: [
+          {
+            name: "index",
+            label: "#",
+            field: "index",
+            align: "center",
+            sortable: true
+          },
+          {
+            name: "chargingTime",
+            label: "日期",
+            field: "chargingTime",
+            sortable: true,
+            align: "center"
+          },
+          {
+            name: "individualPay",
+            label: "个人支付费用",
+            field: "individualPay",
+            align: "center",
+            format: val => `${val}` + `元`,
+            sortable: true
+          },
+          {
+            name: "medicarePay",
+            align: "center",
+            label: "医保支付费用",
+            field: "medicarePay",
+            format: val => `${val}` + `元`,
+            sortable: true
+          },
+          {
+            name: "totalCost",
+            align: "center",
+            label: "医疗总费用",
+            field: "totalCost",
+            format: val => `${val}` + `元`,
+            sortable: true
+          },
+          {
+            name: "ratio",
+            align: "center",
+            label: "医保支付占比",
+            field: "ratio",
+            sortable: true
+          }
+        ],
+        data: []
+      },
+      options: [
+        { value: "individualPay", label: "个人支付" },
+        { value: "medicarePay", label: "医保支付" },
+        { value: "totalCost", label: "医疗总费用" }
+      ],
+
+      pickerOptions: {
+        shortcuts: [
+          {
+            text: "本月",
+            onClick(picker) {
+              picker.$emit("pick", [new Date(), new Date()]);
+            }
+          },
+          {
+            text: "今年至今",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date(new Date().getFullYear(), 0);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近六个月",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setMonth(start.getMonth() - 6);
+              picker.$emit("pick", [start, end]);
+            }
+          }
+        ]
       }
     };
   },
   methods: {
-    onItemClick() {},
-    myEcharts() {
+    async submit() {
+      let parm = {};
+      this.searchParam.type.map(item => {
+        parm[item] = item;
+      });
+      parm.startDate = this.searchParam.chargingTime[0];
+      parm.endDate = getMonthLastDay(this.searchParam.chargingTime[1]);
+      let optionData;
+      await this.$http
+        .post("/fundUse/getFundUseByTime", parm)
+        .then(async res => {
+          if (res.status === 200) {
+            optionData = res.data.data;
+          }
+        })
+        .catch(() => {});
+      let histogramOption = setbyTimeChartOption(optionData);
+      this.changeChart(histogramOption, "byTimeHistogram");
+      this.formatTableData(optionData);
+      let pieOption = setbyTimePieChartOption(optionData);
+      this.changeChart(pieOption, "byTimePieChart");
+    },
+    changeChart(option, id) {
+      let myChart = this.$echarts.init(document.getElementById(id));
+      myChart.setOption(option, true);
+    },
+    drawChart(option, id) {
       // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById("main"));
-      let resizeDiv = document.getElementById("main");
+      let myChart = this.$echarts.init(document.getElementById(id));
+      let resizeDiv = document.getElementById(id);
       // 指定图表的配置项和数据
-      let option = byTimeOption;
       // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
+      myChart.setOption(option, true);
       let linstener = function() {
         myChart.resize();
       };
       EleResize.on(resizeDiv, linstener);
+    },
+    async initialize() {
+      let parm = {
+        totalCost: "totalCost",
+        individualPay: "individualPay",
+        medicarePay: "medicarePay",
+        startDate: "20160701",
+        endDate: "20161231"
+      };
+      let optionData = [];
+      await this.$http
+        .post("/fundUse/getFundUseByTime", parm)
+        .then(async res => {
+          if (res.status === 200) {
+            optionData = res.data.data;
+            let histogramOption = setbyTimeChartOption(optionData);
+            this.drawChart(histogramOption, "byTimeHistogram");
+            this.formatTableData(optionData);
+            let pieOption = setbyTimePieChartOption(optionData);
+            this.drawChart(pieOption, "byTimePieChart");
+          }
+        })
+        .catch(() => {});
+    },
+    formatTableData(optionData) {
+      this.byTimeTable.data = [];
+      for (let i = 0; i < optionData.length; i++) {
+        let temp = {};
+        temp.index = i + 1;
+        temp.individualPay = optionData[i].individualPay;
+        temp.medicarePay = optionData[i].medicarePay;
+        temp.totalCost = optionData[i].totalCost;
+        temp.chargingTime =
+          optionData[i].chargingTime.slice(0, 4) +
+          "-" +
+          optionData[i].chargingTime.slice(4);
+        temp.ratio =
+          ((optionData[i].medicarePay / optionData[i].totalCost) * 100).toFixed(
+            2
+          ) + "%";
+        this.byTimeTable.data.push(temp);
+      }
     }
   },
   mounted() {
-    this.myEcharts();
+    this.initialize();
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .byTIme {
-  #main {
-    // width: 80%;
+  #byTimeHistogram {
     height: 600px;
+  }
+  #byTimePieChart {
+    height: 400px;
   }
   .byTime-searchBar-row {
     .byTime-searchBar-row-col {
@@ -133,4 +281,29 @@ export default {
     }
   }
 }
+</style>
+<style lang="sass">
+.byTimeTable
+  /* height or max-height is important */
+  height: 400px
+
+  .q-table__title
+    font-size: 24px
+  .q-table__top,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #018DA7
+    color: #FFFFFF
+    font-size: 14px
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
 </style>
