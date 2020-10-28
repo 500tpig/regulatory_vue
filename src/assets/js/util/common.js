ed1f34ea30b2658a31130debe65025d12d88ae4f
500tpig <995375558@qq.com>
2020-10-28 15:12:56 +0800

    把时间输入框的通用配置分离出来。

--- src/assets/js/util/common.js
+++ src/assets/js/util/common.js
@@ -48,5 +48,31 @@ function accAdd(arg1, arg2) {
   }
   return (arg1 + arg2) / m;
 }
-
-export { getMonthLastDay, accAdd, formatDate };
+const pickerOptions = {
+  shortcuts: [
+    {
+      text: "本月",
+      onClick(picker) {
+        picker.$emit("pick", [new Date(), new Date()]);
+      }
+    },
+    {
+      text: "今年至今",
+      onClick(picker) {
+        const end = new Date();
+        const start = new Date(new Date().getFullYear(), 0);
+        picker.$emit("pick", [start, end]);
+      }
+    },
+    {
+      text: "最近六个月",
+      onClick(picker) {
+        const end = new Date();
+        const start = new Date();
+        start.setMonth(start.getMonth() - 6);
+        picker.$emit("pick", [start, end]);
+      }
+    }
+  ]
+};
+export { getMonthLastDay, accAdd, formatDate, pickerOptions };
