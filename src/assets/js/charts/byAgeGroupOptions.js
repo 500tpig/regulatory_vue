96b54a18883ec0c8a7ff562bdd1832ed35b57d02
500tpig <995375558@qq.com>
2020-11-15 15:17:23 +0800

    添加年龄组页面。

---
+++ src/assets/js/charts/byAgeGroupOptions.js
@@ -0,0 +1,489 @@
+import { accAdd, calculate } from "../util/common";
+import "assets/js/util/lodash";
+function setAgeGroupChartOption(chartData, isDrillDown) {
+  let ageGroups = [];
+  let individualPay = [];
+  let totalCost = [];
+  let medicarePay = [];
+  let title = {};
+  chartData.map(item => {
+    ageGroups.push(item.ageScope);
+    individualPay.push(item.individualPay);
+    totalCost.push(item.totalCost);
+    medicarePay.push(item.medicarePay);
+  });
+  let Option = {
+    title: title,
+    toolbox: {
+      //可视化的工具箱
+      show: true,
+      right: "15%",
+      top: "2%",
+      feature: {
+        dataView: {
+          //数据视图
+          show: true,
+          readOnly: true,
+          optionToContent: function(opt) {
+            let axisData = opt.xAxis[0].data;
+            let series = opt.series;
+            let tdHeaders = "<td>日期</td>"; //表头
+            series.forEach(function(item) {
+              tdHeaders += "<td>" + item.name + "</td>"; //组装表头
+            });
+            let table =
+              '<div class="table-responsive"><table class="table table-bordered table-striped table-hover" style="text-align:center;color:#222831;"><tbody><tr>' +
+              tdHeaders +
+              "</tr>";
+            let tdBodys = ""; //数据
+            for (let i = 0, l = axisData.length; i < l; i++) {
+              for (let j = 0; j < series.length; j++) {
+                tdBodys += "<td>" + series[j].data[i] + "元</td>"; //组装表数据
+              }
+              table +=
+                '<tr><td style="padding: 0 10px">' +
+                axisData[i] +
+                "</td>" +
+                tdBodys +
+                "</tr>";
+              tdBodys = "";
+            }
+
+            table += "</tbody></table></div>";
+            return table;
+          }
+        },
+        restore: {
+          //重置
+          show: true
+        },
+        saveAsImage: {
+          //保存图片
+          show: true
+        },
+        magicType: {
+          //动态类型切换
+          type: ["bar", "line"]
+        }
+      }
+    },
+    tooltip: {
+      trigger: "axis",
+      formatter: {
+        _custom: {
+          type: "function",
+          display: "<span>ƒ</span> (params)"
+        }
+      },
+      backgroundColor: "#fff",
+      textStyle: {
+        color: "#5c6c7c"
+      },
+      padding: [20, 20],
+      extraCssText: "box-shadow: 1px 0 2px 0 rgba(163,163,163,0.5)"
+    },
+    grid: {
+      top: "10%",
+      bottom: "18%"
+      // x: 40,
+      // y: 40,
+      // x2: 40,
+      // y2: 20
+    },
+    dataZoom: [
+      {
+        show: true,
+        start: 0,
+        end: 100,
+        handleSize: "100%",
+        bottom: "4%",
+        // left: "14%",
+        // right: "30%",
+        handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
+        handleStyle: {
+          /*手柄的样式*/
+          color: "#40bcf9",
+          borderColor: "#1fb2fb"
+        },
+        backgroundColor: "#e2f3ff" /*背景 */,
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: "#ff2e63",
+            width: 2
+          },
+          areaStyle: {
+            color: "#d4d9dd"
+          }
+        },
+        fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/
+      },
+      {
+        show: true,
+        type: "inside",
+        start: 0,
+        end: 100
+      },
+      {
+        handleIcon: "M0,0 v9.7h5 v-9.7h-5 Z",
+        handleStyle: {
+          /*手柄的样式*/
+          color: "#40bcf9",
+          borderColor: "#1fb2fb"
+        },
+        backgroundColor: "#e2f3ff" /*背景 */,
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: "#000000"
+          },
+          areaStyle: {
+            color: "#d4d9dd"
+          }
+        },
+        fillerColor: "rgba(31,178,251,0.2)" /*被start和end遮住的背景*/,
+        show: true,
+        yAxisIndex: 0,
+        filterMode: "empty",
+        width: 20,
+        height: "70%",
+        showDataShadow: false,
+        right: "4%",
+        top: "13%"
+      }
+    ],
+    legend: {
+      data: ["个人支付", "医保支付", "医疗总费用"],
+      x: "center",
+      top: 10
+    },
+    xAxis: {
+      type: "category",
+      data: ageGroups,
+      axisLabel: {
+        // interval: 0,
+        // rotate: 30
+      }
+    },
+    yAxis: [
+      {
+        name: "金额",
+        type: "value",
+        axisLabel: {
+          formatter: function(value, index) {
+            if (value > 100000000)
+              return (value / 100000000).toFixed(2) + "亿元";
+            else if (value > 10000) return value / 10000 + "万元";
+            return value.toFixed(2) + "元";
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "医保支付",
+        type: "bar",
+        stack: "one",
+        barMaxWidth: 40,
+        data: medicarePay,
+        itemStyle: {
+          normal: {
+            color: "#61C378"
+          }
+        }
+      },
+      {
+        name: "个人支付",
+        type: "bar",
+        data: individualPay,
+        barMaxWidth: 40,
+        stack: "one",
+        itemStyle: {
+          normal: {
+            color: "#7367F0",
+            barBorderRadius: [30, 30, 0, 0]
+          }
+        }
+      },
+      {
+        name: "医疗总费用",
+        type: "line",
+        symbolSize: 1,
+        symbol: "circle",
+        barMaxWidth: 40,
+        itemStyle: {
+          normal: {
+            color: "#D95E5A",
+            barBorderRadius: 0
+          }
+        },
+        lineStyle: {
+          normal: {
+            width: 4,
+            itemStyle: {
+              normal: {
+                color: "#D95E5A"
+              }
+            }
+          }
+        },
+        data: totalCost
+      }
+    ]
+  };
+  return Option;
+}
+function setAgeGroupDoubleHistogramOption(doubleHistogramData, type) {
+  let data = {
+    OC: {
+      name: "门诊",
+      data: []
+    },
+    HC: {
+      name: "住院",
+      data: []
+    }
+  };
+  let sum = {
+    OC: 0,
+    HC: 0
+  };
+  for (let key in doubleHistogramData) {
+    let temp = doubleHistogramData[key];
+    temp.map(item => {
+      let tempObj = {};
+      tempObj.value = item[type];
+      sum[key] = accAdd(sum[key], item[type]);
+      tempObj.label = item.ageScope;
+      data[key].data.push(tempObj);
+    });
+  }
+  let yAxisData = new Set();
+  let legendData = [];
+  _.forEach(data, d => {
+    legendData.push(d.name);
+    _.forEach(d.data, item => {
+      yAxisData.add(item.label);
+    });
+  });
+
+  let top = 0;
+  let bottom = 60;
+
+  yAxisData = [...yAxisData];
+
+  let option = {
+    tooltip: {
+      show: true,
+      trigger: "axis",
+      formatter: function(parms) {
+        var str =
+          parms[0].marker +
+          parms[0].seriesName +
+          " " +
+          parms[0].name +
+          "</br>" +
+          "金额：" +
+          parms[0].value +
+          "元</br>";
+        return str;
+      },
+      axisPointer: {
+        type: "shadow"
+      }
+    },
+    legend: {
+      left: "center",
+      bottom: 24,
+      itemWidth: 15,
+      itemHeight: 11,
+      itemGap: 20,
+      borderRadius: 4,
+      textStyle: {
+        color: "#262C41",
+        fontSize: 14
+      },
+      data: legendData
+    },
+    grid: [
+      {
+        left: "10%",
+        width: "30%",
+        containLabel: true,
+        bottom
+      },
+      {
+        left: "50%",
+        width: "0%",
+        bottom: bottom + 16
+      },
+      {
+        right: "10%",
+        width: "30%",
+        containLabel: true,
+        bottom
+      }
+    ].map(item =>
+      _.merge(
+        {
+          top
+        },
+        item
+      )
+    ),
+    xAxis: [
+      {
+        type: "value",
+        inverse: true,
+        axisLabel: {
+          show: true
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        splitLine: {
+          show: true
+        }
+      },
+      {
+        gridIndex: 1,
+        show: true
+      },
+      {
+        gridIndex: 2,
+        type: "value",
+        axisLabel: {
+          show: true
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        splitLine: {
+          show: true
+        }
+      }
+    ].map(item =>
+      _.merge(item, {
+        axisLabel: {
+          color: "#949AA8",
+          margin: 0
+        },
+        splitLine: {
+          lineStyle: {
+            color: "#E0E0E0",
+            type: "dashed"
+          }
+        }
+      })
+    ),
+    yAxis: [
+      {
+        position: "right",
+        axisLabel: {
+          show: false
+        },
+        axisLine: {
+          show: true
+        }
+      },
+      {
+        gridIndex: 1,
+        position: "center",
+        axisLabel: {
+          align: "center",
+          padding: [8, 0, 0, 0],
+          fontSize: 12,
+          color: `#262C41`
+        },
+        axisLine: {
+          show: false
+        }
+      },
+      {
+        gridIndex: 2,
+        position: "left",
+        axisLabel: {
+          show: false
+        },
+        axisLine: {
+          show: true
+        }
+      }
+    ].map(item => {
+      return _.merge(item, {
+        type: "category",
+        inverse: false,
+        axisLine: {
+          lineStyle: {
+            color: "#E0E0E0"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        data: yAxisData
+      });
+    }),
+    series: [
+      {
+        name: _.get(data, "OC.name"),
+        label: {
+          position: "left",
+          formatter: function(a) {
+            let value = a.value;
+            if (value > 100000000)
+              return calculate.Div(value, 100000000, 2) + "亿元";
+            else if (value > 10000)
+              return calculate.Div(value, 10000, 2) + "万元";
+            return value.toFixed(2) + "元";
+          }
+        },
+        itemStyle: {
+          color: "#01C5B2",
+          barBorderRadius: [4, 0, 0, 4]
+        },
+        data: _.map(_.get(data, "OC.data"), d => d.value)
+      },
+      {
+        xAxisIndex: 2,
+        yAxisIndex: 2,
+        name: _.get(data, "HC.name"),
+        label: {
+          position: "right",
+          formatter: function(a) {
+            let value = a.value;
+            if (value > 100000000)
+              return calculate.Div(value, 100000000, 2) + "亿元";
+            else if (value > 10000)
+              return calculate.Div(value, 10000, 2) + "万元";
+            return value.toFixed(2) + "元";
+          }
+        },
+        itemStyle: {
+          color: "#FB6F6C",
+          barBorderRadius: [0, 4, 4, 0]
+        },
+        data: _.map(_.get(data, "HC.data"), d => d.value)
+      }
+    ].map(item =>
+      _.merge(item, {
+        type: "bar",
+        barWidth: 11,
+        label: {
+          show: true,
+          fontFamily: "Rubik-Medium",
+          fontSize: 14,
+          distance: 10
+        }
+      })
+    )
+  };
+  return option;
+}
+export { setAgeGroupChartOption, setAgeGroupDoubleHistogramOption };
--- src/pages/Foundation/byAgeGroup.vue
+++ src/pages/Foundation/byAgeGroup.vue
@@ -1,14 +1,192 @@
 <template>
-  <q-page class="">
-    <page-base-scroll content_class="q-mb-xl"> </page-base-scroll>
+  <q-page class="byAgeGroup">
+    <page-base-scroll content_class="q-mb-xl q-pt-md ">
+      <div class="row justify-center">
+        <q-expansion-item
+          class="shadow-1 overflow-hidden col-8"
+          style="border-radius: 30px"
+          icon="icon-icon_shaixuan"
+          label="筛选条件"
+          v-model="common.expanded[0]"
+          header-class="bg-primary text-white text-weight-medium"
+          expand-icon-class="text-white"
+        >
+          <q-card class="q-pt-md text-grey-8 text-weight-medium text-subtitle1">
+            <q-card-section horizontal class="items-center q-pl-xl cardSection">
+              <div class="q-mr-sm">类型选择:</div>
+              <div class="text-grey-8" style="font-size:14px;">
+                <q-radio v-model="searchParam.type" val="OC" label="门诊费用" />
+                <q-radio v-model="searchParam.type" val="HC" label="住院费用" />
+                <q-radio
+                  v-model="searchParam.type"
+                  val="OCAndHC"
+                  label="全选"
+                />
+              </div>
+              <span class="q-mx-md">日期选择:</span>
+              <div style="width:30%;">
+                <el-date-picker
+                  align="center"
+                  value-format="yyyyMMdd"
+                  v-model="searchParam.chargingTime"
+                  type="daterange"
+                  unlink-panels
+                  clearable
+                  start-placeholder="开始月份"
+                  end-placeholder="结束月份"
+                  :picker-options="common.pickerOptions"
+                  style="width:100%;"
+                />
+              </div>
+            </q-card-section>
+            <q-card-actions align="right">
+              <q-btn
+                class="q-mr-lg"
+                flat
+                style="font-size:16px;"
+                @click="initialize"
+                >提交</q-btn
+              >
+            </q-card-actions>
+          </q-card>
+        </q-expansion-item>
+      </div>
+      <div class="row">
+        <div class="col-8 offset-2 ">
+          <q-card class="q-mt-lg q-pt-md">
+            <div id="byAgeGroupHistogram"></div>
+          </q-card>
+        </div>
+      </div>
+      <div class="row justify-center">
+        <div class="col-9">
+          <q-card class="q-mt-lg q-pt-md">
+            <div id="byAgeGroupDoubleHistogram"></div>
+          </q-card>
+        </div>
+      </div>
+    </page-base-scroll>
   </q-page>
 </template>
 
 <script>
 import pageBaseScroll from "components/utils/PageScroll";
+import {
+  setAgeGroupChartOption,
+  setAgeGroupDoubleHistogramOption
+} from "assets/js/charts/byAgeGroupOptions";
+import {
+  pickerOptions,
+  shallowCopyObj,
+  formatDate
+} from "assets/js/util/common";
+import { EleResize } from "assets/js/util/esresize";
 export default {
-  components: { pageBaseScroll: pageBaseScroll }
+  components: { pageBaseScroll },
+  data() {
+    return {
+      searchParam: {
+        chargingTime: ["20160701", "20161231"],
+        type: "OC",
+        isDrillDown: "",
+        ageGroup: ""
+      },
+      common: {
+        expanded: [true],
+        isInitialize: true,
+        pickerOptions: pickerOptions
+      }
+    };
+  },
+  mounted() {
+    this.initialize();
+  },
+  methods: {
+    // 初始化请求
+    async initialize() {
+      let param = {};
+
+      param = shallowCopyObj(this.searchParam, param);
+      param.startDate = this.searchParam.chargingTime[0];
+      param.endDate = this.searchParam.chargingTime[1];
+
+      let optionData = [];
+      await this.$http
+        .post("/fundUse/monthlyFeeByAgeGroup", param)
+        .then(res => {
+          if (res.status === 200) {
+            optionData = res.data.data;
+          }
+        })
+        .catch(e => {});
+      let doubleHistogramData = [];
+      await this.$http
+        .post("/fundUse/monthlyFeeByAgeGroupOCAndHC", param)
+        .then(res => {
+          if (res.status === 200) {
+            doubleHistogramData = res.data.data;
+          }
+        })
+        .catch(e => {});
+      this.afterHttp(optionData, doubleHistogramData);
+    },
+    // 画图表
+    drawChart(option, id) {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById(id));
+      let resizeDiv = document.getElementById(id);
+      // 指定图表的配置项和数据
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option, true);
+      let that = this;
+      if (this.common.isInitialize) {
+        let histogramChart = this.$echarts.init(
+          document.getElementById("byAgeGroupHistogram")
+        );
+        let doubleHistogram = this.$echarts.init(
+          document.getElementById("byAgeGroupDoubleHistogram")
+        );
+
+        let linstener = function() {
+          histogramChart.resize();
+          doubleHistogram.resize();
+        };
+        EleResize.on(resizeDiv, linstener);
+        this.common.isInitialize = false;
+      }
+    },
+    // 处理请求后的结果
+    afterHttp(optionData, doubleHistogramData) {
+      let histogramOption = setAgeGroupChartOption(optionData);
+      this.drawChart(histogramOption, "byAgeGroupHistogram");
+
+      let doubleHistogramOption = setAgeGroupDoubleHistogramOption(
+        doubleHistogramData,
+        "totalCost"
+      );
+      this.drawChart(doubleHistogramOption, "byAgeGroupDoubleHistogram");
+    }
+  }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.byAgeGroup {
+  .cardSection {
+    // height: 70px;
+    .el-range-editor.is-active,
+    .el-range-editor.is-active:hover {
+      border: 2px solid $primary;
+    }
+    .el-input__inner {
+      border: 1px solid rgba(0, 0, 0, 0.24);
+    }
+  }
+  #byAgeGroupHistogram {
+    height: 600px;
+  }
+  #byAgeGroupDoubleHistogram {
+    height: 630px;
+  }
+}
+</style>
