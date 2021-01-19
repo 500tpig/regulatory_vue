0ba803debc4d6489584e34539845475038d5cfde
500tpig <995375558@qq.com>
2021-01-19 20:26:35 +0800

    添加表格导出csv的方法。

--- src/assets/js/util/common.js
+++ src/assets/js/util/common.js
@@ -1,3 +1,4 @@
+import { exportFile } from "quasar";
 function getMonthLastDay(str) {
   let year = str.substring(0, 4);
   let month = str.substring(4, 6);
@@ -363,6 +364,53 @@ function isObjArr(value) {
     return "error";
   }
 }
+
+function wrapCsvValue(val, formatFn) {
+  let formatted = formatFn !== void 0 ? formatFn(val) : val;
+
+  formatted =
+    formatted === void 0 || formatted === null ? "" : String(formatted);
+
+  formatted = formatted.split('"').join('""');
+  /**
+   * Excel accepts \n and \r in strings, but some other CSV parsers do not
+   * Uncomment the next two lines to escape new lines
+   */
+  // .split('\n').join('\\n')
+  // .split('\r').join('\\r')
+
+  return `"${formatted}"`;
+}
+
+function exportTable(columns, data) {
+  // naive encoding to csv format
+  const content = [columns.map(col => wrapCsvValue(col.label))]
+    .concat(
+      data.map(row =>
+        columns
+          .map(col =>
+            wrapCsvValue(
+              typeof col.field === "function"
+                ? col.field(row)
+                : row[col.field === void 0 ? col.name : col.field],
+              col.format
+            )
+          )
+          .join(",")
+      )
+    )
+    .join("\r\n");
+
+  const status = exportFile("table-export.csv", content, "text/csv");
+
+  if (status !== true) {
+    this.$q.notify({
+      message: "Browser denied file download...",
+      color: "negative",
+      icon: "warning"
+    });
+  }
+}
 export {
   getMonthLastDay,
   accAdd,
@@ -377,5 +425,6 @@ export {
   getYearBetween,
   quick,
   getLastDay,
-  isObjArr
+  isObjArr,
+  exportTable
 };
